8c42e48c1725c91ae9bef36a88d346850e067392
Tony1704 <anton.ochel1@gmail.com>
2020-03-19 21:06:28 +0100

    email and pw validation for form

--- Source-Code/fagb-frontend/src/app/login/login.component.ts
+++ Source-Code/fagb-frontend/src/app/login/login.component.ts
@@ -7,16 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public emailValid: boolean;
+  public pwEntered: boolean;
+  private stayLoggedIn: boolean;
+  public formIsInitial: boolean;
+  regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
   constructor() { }
 
   ngOnInit(): void {
+    this.emailValid = true;
+    this.pwEntered= true;
+    this.formIsInitial = true;
   }
 
-  toggle(): void{
-    if(document.getElementById("login-modal").className == "modal"){
-    document.getElementById("login-modal").className = "modal is-active";
-    }else{
+  toggle(): void {
+    if (document.getElementById("login-modal").className == "modal") {
+      document.getElementById("login-modal").className = "modal is-active";
+    } else {
       document.getElementById("login-modal").className = "modal";
     }
   }
+
+  validateEmail(event: any){
+    this.formIsInitial = false;
+    if(this.regexp.test(event.target.value)){
+      this.emailValid = true;
+    }else{
+      this.emailValid = false;
+      console.log("emailfalsch")
+    }
+  }
+
+  validatePassword(event: any){
+    this.formIsInitial = false;
+    if(event.target.value == "" || event.target.value == null){
+      this.pwEntered = false;
+      console.log("pwfalsch")
+    }else{
+      this.pwEntered = true;
+    }
+  }
+
+  login(): void {
+
+
+  }
+
 }
