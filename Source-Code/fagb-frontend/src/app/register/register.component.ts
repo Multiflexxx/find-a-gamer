114b8ba51f740d3660355ed3e069eb3e23bd378d
TristanMrb <trisemig@aol.com>
2020-03-19 21:00:12 +0100

    Add post and form

--- Source-Code/fagb-frontend/src/app/register/register.component.ts
+++ Source-Code/fagb-frontend/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroupDirective, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { HttpClient } from '@angular/common/http';
 
 
 /** Error when invalid control is dirty, touched, or submitted. */
@@ -18,17 +19,50 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  // stepper
   isLinear = true;
   hide = true;
   profileFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  @ViewChild('profilestep') profilestep;
-
   emailControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
+
+// localhost:3060
+
+  url = 'http://httpbin.org/post';
+  json;
+
+  constructor(private _formBuilder: FormBuilder, private http: HttpClient) {
+    this.profileFormGroup = this._formBuilder.group({
+      nameCtrl: ['', Validators.required],
+      tagCtrl: ['', Validators.required],
+      passCtrl: ['', Validators.required],
+      rpassCtrl: ['', Validators.required],
+      mailCtrl: ['', Validators.required]
+    });
+    
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    
+  }
+
+  onSubmit(userData) {
+    // Process checkout data here
+    // this.profileFormGroup.reset();
+
+    this.http.post(this.url, userData).toPromise().then((data:any) => {
+      console.log(data);
+      this.json = JSON.stringify(data.json);
+    });
+    console.log('Form Test', userData);
+  }
 
   getErrorMessage() {
     if (this.emailControl.hasError('required')) {
@@ -36,22 +70,6 @@ export class RegisterComponent implements OnInit {
     }
 
     return this.emailControl.hasError('email') ? 'Not a valid email' : '';
-  }
-
-
-  constructor(private _formBuilder: FormBuilder) {}
-
-  ngOnInit() {
-    this.profileFormGroup = this._formBuilder.group({
-      nameCtrl: ['', Validators.required],
-      tagCtrl: ['', Validators.required],
-      passCtrl: ['', Validators.required],
-      rpassCtrl: ['', Validators.required]
-    });
-    
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
   }
 
 }
