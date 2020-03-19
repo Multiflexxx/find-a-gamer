cdbd6c0ce026bab8f7782db8de141a47f3486bb2
NerdyStuff <NerdyStuff@users.noreply.github.com>
2020-03-19 18:16:08 +0100

    Update backend

--- Source-Code/fagb-backend/src/data_objects/notifymatch.ts
+++ Source-Code/fagb-backend/src/data_objects/notifymatch.ts
@@ -1,3 +1,3 @@
 export class NotifyMatch {
-    userID: string;
+    public userID: string;
 }
--- Source-Code/fagb-backend/src/notifymatchendpoint/notifymatchendpoint.controller.ts
+++ Source-Code/fagb-backend/src/notifymatchendpoint/notifymatchendpoint.controller.ts
@@ -1,22 +1,22 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { NotifyMatch } from '../data_objects/notifymatch';
 import { ConnectToDatabaseService } from '../connecttodatabase/connecttodatabase.service';
 
 @Controller('notifymatchendpoint')
 export class NotifymatchendpointController {
 
-    flagIsSet: boolean = false;
-    matchedUserID: String = "";
+    flagIsSet: boolean = true;
+    matchedUserID: String = "123";
     database: ConnectToDatabaseService
 
     @Post()
-    handleUpdate(@Body() notifyMatch: NotifyMatch) {
+    handleUpdate(@Body() notifyMatch: NotifyMatch): string {
         console.log(notifyMatch.userID);
 
         // check database
-        this.database = new ConnectToDatabaseService;
+        //this.database = new ConnectToDatabaseService;
 
-        this.database.getResult('SELECT `PersonID` FROM FindAGamingBuddy.Persons WHERE `PersonID` = ' + notifyMatch.userID + ';');
+        //this.database.getResult('SELECT `PersonID` FROM FindAGamingBuddy.Persons WHERE `PersonID` = ' + notifyMatch.userID + ';');
 
         if (this.flagIsSet) {
             return '{"matchFound":true, "matchedUserID":"' + this.matchedUserID + '"}';
