144f104ed73fc6a520876a0d059a685a295091ed
TristanMrb <trisemig@aol.com>
2020-03-23 01:16:50 +0100

    Update registration and theme

--- Source-Code/fagb-frontend/src/app/register/register.component.ts
+++ Source-Code/fagb-frontend/src/app/register/register.component.ts
@@ -8,11 +8,15 @@ import { Game } from '../data_objects/game';
 import { Language } from '../data_objects/language';
 import { Region } from '../data_objects/region';
 import { emailValidator } from '../shared/email-validator.directive';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: { displayDefaultIndicatorType: false }
+  }]
 })
 export class RegisterComponent implements OnInit {
   // stepper
@@ -21,6 +25,9 @@ export class RegisterComponent implements OnInit {
   profileForm: FormGroup;
   gameForm: FormGroup;
 
+  public startDate = new Date(2000, 0, 1);
+  public minDate: Date;
+  public maxDate: Date;
   private profileData: any;
   private gameData: any;
   //url = 'http://httpbin.org/post';
@@ -28,7 +35,7 @@ export class RegisterComponent implements OnInit {
   json;
 
   // RegExp
-  regDisTag : string = '[a-zA-Z0-9]{2,32}#[0-9]{4}';
+  regDisTag: string = '[a-zA-Z0-9]{2,32}#[0-9]{4}';
 
   regionList = [
     {
@@ -41,30 +48,41 @@ export class RegisterComponent implements OnInit {
     },
     {
       id: 2,
-      name: 'North America'
+      name: 'Europa'
     },
     {
       id: 3,
-      name: 'South America'
+      name: 'North America'
     },
     {
       id: 4,
-      name: 'Africa'
+      name: 'South America'
     }
   ];
 
   langList = [
     {
       id: 0,
-      name: 1
+      name: 'English'
     },
     {
       id: 1,
-      name: 'german'
+      name: 'German'
     }
   ];
 
+
+  toppings = new FormControl();
+
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
   constructor(private http: HttpClient) {
+    // Set the minimum to January 1st 20 years in the past and December 31st a year in the future.
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 100, 0, 1);
+    this.maxDate = new Date(currentYear - 6, 11, 31);
+
+
     this.profileForm = new FormGroup({
       nameCtrl: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(32)]),
       tagCtrl: new FormControl('', [Validators.required, Validators.pattern(this.regDisTag)]),
@@ -73,7 +91,7 @@ export class RegisterComponent implements OnInit {
       regionCtrl: new FormControl('', Validators.required),
       langCtrl: new FormControl('', Validators.required),
       passCtrl: new FormControl('', Validators.required),
-      rpassCtrl: new FormControl('', Validators.required)
+      rpassCtrl: new FormControl('', Validators.required),
     });
 
     this.gameForm = new FormGroup({
@@ -84,7 +102,7 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public hasError = (controlName: string, errorName: string) =>{
+  public hasError = (controlName: string, errorName: string) => {
     return this.profileForm.controls[controlName].hasError(errorName);
   }
 
