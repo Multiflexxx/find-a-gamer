cb60258bdacdcc4c2dd25da6fa3ef3e73833f690
TristanMrb <trisemig@aol.com>
2020-03-24 14:22:43 +0100

    Update

--- Source-Code/fagb-frontend/src/app/register/register.component.ts
+++ Source-Code/fagb-frontend/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormGroupDirective, NgForm, FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 // Import data objects
@@ -9,6 +9,7 @@ import { Language } from '../data_objects/language';
 import { Region } from '../data_objects/region';
 import { emailValidator } from '../shared/email-validator.directive';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { compareValidator } from '../shared/compare-validator.directive';
 
 @Component({
   selector: 'app-register',
@@ -19,26 +20,33 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
   }]
 })
 export class RegisterComponent implements OnInit {
-  // stepper
+  // Stepper values
   isEditable = false;
   hide = true;
+
+  // ReactiveForms
   profileForm: FormGroup;
   gameForm: FormGroup;
 
+  // Datepicker values
   public startDate = new Date(2000, 0, 1);
   public minDate: Date;
   public maxDate: Date;
+
   private profileData: any;
   private gameData: any;
-  //url = 'http://httpbin.org/post';
-  url = 'http://localhost:3000/registrationendpoint';
-  json;
 
   // RegExp
   regDisTag: string = '[a-zA-Z0-9]{2,32}#[0-9]{4}';
   strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
   mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
 
+
+  //url = 'http://httpbin.org/post';
+  url = 'http://localhost:3000/registrationendpoint';
+  json;
+
+  // Backend input
   regionList = [
     {
       id: 0,
@@ -73,13 +81,44 @@ export class RegisterComponent implements OnInit {
     }
   ];
 
-  constructor(private http: HttpClient) {
+
+
+  // constructor(private http: HttpClient) {
+  //   // Set the minimum to January 1st 20 years in the past and December 31st a year in the future.
+  //   const currentYear = new Date().getFullYear();
+  //   this.minDate = new Date(currentYear - 100, 0, 1);
+  //   this.maxDate = new Date(currentYear - 6, 11, 31);
+
+
+  //   this.profileForm = new FormGroup({
+  //     nameCtrl: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(32)]),
+  //     tagCtrl: new FormControl('', [Validators.required, Validators.pattern(this.regDisTag)]),
+  //     mailCtrl: new FormControl('', [Validators.required, emailValidator()]),
+  //     dateCtrl: new FormControl('', Validators.required),
+  //     regionCtrl: new FormControl('', Validators.required),
+  //     langCtrl: new FormControl('', Validators.required),
+  //     passCtrl: new FormControl('', Validators.required),
+  //     rpassCtrl: new FormControl('', Validators.required),
+  //   });
+
+  //   this.gameForm = new FormGroup({
+  //     gameCtrl: new FormControl('', Validators.required)
+  //   });
+  // }
+
+  constructor(private formBuilder: FormBuilder ,private http: HttpClient) {
+    
     // Set the minimum to January 1st 20 years in the past and December 31st a year in the future.
     const currentYear = new Date().getFullYear();
     this.minDate = new Date(currentYear - 100, 0, 1);
     this.maxDate = new Date(currentYear - 6, 11, 31);
+  }
 
+  ngOnInit(): void {
+    this.createForm();
+  }
 
+  createForm() {
     this.profileForm = new FormGroup({
       nameCtrl: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(32)]),
       tagCtrl: new FormControl('', [Validators.required, Validators.pattern(this.regDisTag)]),
@@ -88,7 +127,7 @@ export class RegisterComponent implements OnInit {
       regionCtrl: new FormControl('', Validators.required),
       langCtrl: new FormControl('', Validators.required),
       passCtrl: new FormControl('', Validators.required),
-      rpassCtrl: new FormControl('', Validators.required),
+      rpassCtrl: new FormControl('', [Validators.required, compareValidator('passCtrl')]),
     });
 
     this.gameForm = new FormGroup({
@@ -96,22 +135,52 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
+  get nameCtrl() {
+    return this.profileForm.get('nameCtrl');
   }
+
+  get tagCtrl() {
+    return this.profileForm.get('tagCtrl');
+  }
+
+  get mailCtrl() {
+    return this.profileForm.get('mailCtrl');
+  }
+
+  get dateCtrl() {
+    return this.profileForm.get('dateCtrl');
+  }
+
+  get regionCtrl() {
+    return this.profileForm.get('regionCtrl');
+  }
+
+  get langCtrl() {
+    return this.profileForm.get('langCtrl');
+  }
+
+  get passCtrl() {
+    return this.profileForm.get('passCtrl');
+  }
+
+  get pwConfirm() {
+    return this.profileForm.get('rpassCtrl');
+  }
+
 
   public hasError = (controlName: string, errorName: string) => {
     return this.profileForm.controls[controlName].hasError(errorName);
   }
 
-  public isStrong (controlName: string) : number {
+  public isStrong(controlName: string): number {
     let strenght = -1;
     let pw = this.profileForm.controls[controlName].value;
-    if(this.mediumRegex.test(pw)) {
+    if (this.mediumRegex.test(pw)) {
       strenght = 1;
       console.log(controlName);
     } else if (this.strongRegex.test(pw)) {
       strenght = 2;
-    } else if (pw != ""){
+    } else if (pw != "") {
       strenght = 0;
     }
     return strenght;
