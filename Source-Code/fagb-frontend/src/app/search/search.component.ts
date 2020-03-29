0ed3d41517dd3f8c62643d9c3347695accccd3db
unknown <pizzajojomgh@gmail.com>
2020-03-29 22:46:27 +0200

    save changes

--- Source-Code/fagb-frontend/src/app/search/search.component.ts
+++ Source-Code/fagb-frontend/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  playerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  playerList = [
+    {
+    id:1
+  },
+  {
+    id: 2
+  },
+  {
+    id:3
+  },
+  {
+    id:4
+  },
+  {
+    id:5
+  }
+]
+  constructor(private formBuilder: FormBuilder) { 
+    
   }
 
+  ngOnInit(): void {
+    this.createForm();
+  }
+  createForm(){
+    this.playerForm = this.formBuilder.group({
+      player: ['', [Validators.required]]
+    });
+  }
+  public hasError = (controlName: string, errorName: string) => {
+    return this.playerForm.controls[controlName].hasError(errorName);
+  }
+  
+  
 }
