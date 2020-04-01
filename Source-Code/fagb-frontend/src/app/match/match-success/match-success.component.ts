d4af0791ed9e2001962fc915fb90baf7f571e94f
TristanMrb <trisemig@aol.com>
2020-04-02 01:42:05 +0200

    Add compo

---
+++ Source-Code/fagb-frontend/src/app/match/match-success/match-success.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-match-success',
+  templateUrl: './match-success.component.html',
+  styleUrls: ['./match-success.component.scss']
+})
+export class MatchSuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Source-Code/fagb-frontend/src/app/match/match.module.ts
+++ Source-Code/fagb-frontend/src/app/match/match.module.ts
@@ -7,12 +7,14 @@ import { SharedComponentsModule } from '../shared-components.module';
 import { MatchRoutingModule } from './match-routing.module';
 import { MatchSearchComponent } from './match-search/match-search.component';
 import { MatchProcessComponent } from './match-process/match-process.component';
+import { MatchSuccessComponent } from './match-success/match-success.component';
 
 
 @NgModule({
   declarations: [
     MatchSearchComponent,
     MatchProcessComponent,
+    MatchSuccessComponent,
   ],
   imports: [
     CommonModule,
