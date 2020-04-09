6a34d162294b550425b2c4b18c301c1cae182921
TristanMrb <trisemig@aol.com>
2020-04-09 16:19:53 +0200

    Update game cards

--- Source-Code/fagb-frontend/src/app/landing-page/landing-page.component.ts
+++ Source-Code/fagb-frontend/src/app/landing-page/landing-page.component.ts
@@ -12,54 +12,6 @@ export class LandingPageComponent implements OnInit {
 
   public gameList: Array<GameResponse> = [];
 
-  // items = [
-  //   {
-  //     id: 0,
-  //     name: 'Age of Empires 2',
-  //     img: 'assets\\img\\Age-of-Empires-2.jpg'
-  //   },
-  //   {
-  //     id: 1,
-  //     name: 'Rocket League',
-  //     img: 'assets\\img\\Rocket-League.jpg'
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Apex Legends',
-  //     img: 'assets\\img\\Apex-Legends.jpg'
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Age of Empires 2',
-  //     img: 'assets\\img\\Age-of-Empires-2.jpg'
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'Rocket League',
-  //     img: 'assets\\img\\Rocket-League.jpg'
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Apex Legends',
-  //     img: 'assets\\img\\Apex-Legends.jpg'
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Apex Legends',
-  //     img: 'assets\\img\\Apex-Legends.jpg'
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Apex Legends',
-  //     img: 'assets\\img\\Apex-Legends.jpg'
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Apex Legends',
-  //     img: 'assets\\img\\Apex-Legends.jpg'
-  //   }
-  // ];
-
   public constructor(private gameService: GameService) { }
 
   public ngOnInit(): void {
