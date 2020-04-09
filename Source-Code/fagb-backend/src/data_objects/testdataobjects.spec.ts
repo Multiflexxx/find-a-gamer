226d582ec2512b78071483c06880706fa38b6815
NerdyStuff <NerdyStuff@users.noreply.github.com>
2020-04-10 00:45:10 +0200

    Added unit tests for language and session objects

--- Source-Code/fagb-backend/src/data_objects/testdataobjects.spec.ts
+++ Source-Code/fagb-backend/src/data_objects/testdataobjects.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import {Login} from './login';
+import { Login } from './login';
+import { Session } from './session';
+import { Language } from './language';
+
+// unit tests done for:
+// login
+// session
+// language
+
 
 describe('Check Login-object', () => {
-    let loginPw: Login;
-    let loginSession: Login;
-    let loginSessionEmpty: Login;
-    let loginNull: Login;
-
-    loginPw = new Login(null, 'mail@mail.com', 'hash123', true);
-    loginSession = new Login('session_id');
-    loginSessionEmpty = new Login('');
-    loginNull = new Login();
+    let loginPw: Login = new Login(null, 'mail@mail.com', 'hash123', true);
+    let loginSession: Login = new Login('session_id');
+    let loginSessionEmpty: Login = new Login('');
+    let loginNull: Login = new Login();
 
     it('should be a full valid Login-object', () => {
         expect(loginPw.session_id).toBeNull();
@@ -38,5 +41,51 @@ describe('Check Login-object', () => {
         expect(loginNull.email).toBeNull();
         expect(loginNull.password_hash).toBeNull();
         expect(loginNull.stay_logged_in).toBeFalsy();
+    });
+});
+
+describe('Check Session-object', () => {
+
+    let testDate: Date = new Date();
+    let sessionWithDate: Session = new Session('session_id', 12, true, testDate);
+    let sessionWithoutDate: Session = new Session('session_id_2', 13, true);
+
+    it('should be a Session-object with expiration date', () => {
+        expect(sessionWithDate.session_id).toEqual('session_id');
+        expect(sessionWithDate.user_id).toEqual(12);
+        expect(sessionWithDate.stay_logged_in).toBeTruthy();
+        expect(sessionWithDate.expiration_date).toEqual(testDate);
+    });
+
+    it('should be a Session-object without expiration date', () => {
+        expect(sessionWithoutDate.session_id).toEqual('session_id_2');
+        expect(sessionWithoutDate.user_id).toEqual(13);
+        expect(sessionWithoutDate.stay_logged_in).toBeTruthy();
+        expect(sessionWithoutDate.expiration_date).toBeNull();
+    });
+});
+
+describe('Check Language-object', () => { 
+
+    let languageFull: Language = new Language(1, 'German', 'DE');
+    let languageName: Language = new Language(2, 'English');
+    let languageCode: Language = new Language(3, null, 'US');
+
+    it('should be a full Language-object', () => {
+        expect(languageFull.language_id).toEqual(1);
+        expect(languageFull.name).toEqual('German');
+        expect(languageFull.language_code).toEqual('DE');
+    });
+
+    it('should be a Language-object with language name', () => {
+        expect(languageName.language_id).toEqual(2);
+        expect(languageName.name).toEqual('English');
+        expect(languageName.language_code).toBeNull();
+    });
+
+    it('should be a Language-object with language name', () => {
+        expect(languageCode.language_id).toEqual(3);
+        expect(languageCode.name).toBeNull();
+        expect(languageCode.language_code).toEqual('US');
     });
 });
