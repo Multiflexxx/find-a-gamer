cda7f4576334f13eb35fa9bc8e531327a5a68376
NerdyStuff <NerdyStuff@users.noreply.github.com>
2020-04-10 02:24:07 +0200

    Added unit tests for user language pair, registrationresponse & user; fixed test for region

--- Source-Code/fagb-backend/src/data_objects/testdataobjects.spec.ts
+++ Source-Code/fagb-backend/src/data_objects/testdataobjects.spec.ts
@@ -8,6 +8,9 @@ import { GameResponse } from './gameresponse';
 import { Region } from './region';
 import { Registration } from './registration';
 import { Response } from './response';
+import { UserLanguagePair } from './userlanguagepair';
+import { RegistrationResponse } from './registrationresponse';
+import { User } from './user';
 
 // unit tests done for:
 // login
@@ -19,6 +22,9 @@ import { Response } from './response';
 // region
 // registration
 // response
+// registrationresponse
+// userlanguagepair
+// user
 
 
 describe('Check Login-object', () => {
@@ -149,10 +155,16 @@ describe('Check GameResponse-object', () => {
 
 describe('Check Region-object', () => {
     let region: Region = new Region(3, 'Asia');
+    let regionNull: Region = new Region(4);
 
     it('should be a valid region', () => {
         expect(region.region_id).toEqual(3);
         expect(region.name).toEqual('Asia');
+    });
+
+    it('should be a region without name', () => {
+        expect(regionNull.region_id).toEqual(4);
+        expect(regionNull.name).toBeUndefined();
     });
 });
 
@@ -182,5 +194,72 @@ describe('Check general Response-object', () => {
     it('should be a valid response', () => {
         expect(response.successful).toBeTruthy();
         expect(response.message).toEqual('message');
+    });
+});
+
+describe('Check UserLanguagePair-object', () => { 
+    let userLanguagePair: UserLanguagePair = new UserLanguagePair(5, 6, 3);
+
+    it('should be a valid User-Language-Pair', () => {
+        expect(userLanguagePair.pair_id).toEqual(5);
+        expect(userLanguagePair.user_id).toEqual(6);
+        expect(userLanguagePair.language_id).toEqual(3);
+    });
+});
+
+describe('Check RegistrationResponse-object', () => { 
+    let testDate: Date = new Date();
+    let testSession: Session = new Session('session_id', 12, true, testDate);
+    let registrationResponse: RegistrationResponse = new RegistrationResponse(true, testSession, 'message');
+
+    it('should be a valid registration response', () => {
+        expect(registrationResponse.successful).toBeTruthy();
+        expect(registrationResponse.session_object).toEqual(testSession);
+        expect(registrationResponse.session_object.expiration_date).toEqual(testDate);
+        expect(registrationResponse.message).toEqual('message');
+    });
+});
+
+describe('Check User-object', () => { 
+
+    let cakeDate: Date = new Date();
+    let birthDate: Date = new Date();
+    let testRegion: Region = new Region(3, 'Asia');
+    let testLanguages: Language[] = [new Language(1, 'German', 'DE'), new Language(2, 'English', 'US')];
+    let testGames: Game[] = [new Game(2), new Game(3)];
+    
+    let userFull: User = new User(12, 'mail@mail.com', 'hash123', 'nickname', 'nickname#1234', '/image/img.png', cakeDate, birthDate, 'Bio', testRegion, testGames, testLanguages);
+    let user: User = new User(13, 'mail@mail1.com', 'hash1234', 'nickname', 'nickname#1234', '/image/img.png', cakeDate, birthDate, 'Bio');
+
+    it('should be a full valid user', () => {
+        expect(userFull).toBeDefined();
+        expect(userFull.user_id).toEqual(12);
+        expect(userFull.email).toEqual('mail@mail.com');
+        expect(userFull.password_hash).toEqual('hash123');
+        expect(userFull.nickname).toEqual('nickname');
+        expect(userFull.discord_tag).toEqual('nickname#1234');
+        expect(userFull.profile_picture).toEqual('/image/img.png');
+        expect(userFull.cake_day).toEqual(cakeDate);
+        expect(userFull.birthdate).toEqual(birthDate);
+        expect(userFull.biography).toEqual('Bio');
+        expect(userFull.region).toEqual(testRegion);
+        expect(userFull.games).toEqual(testGames);
+        expect(userFull.languages).toEqual(testLanguages);
+    });
+
+    it('should be a user without languages and games', () => {
+        expect(user).toBeDefined();
+        expect(user.user_id).toEqual(13);
+        expect(user.email).toEqual('mail@mail1.com');
+        expect(user.password_hash).toEqual('hash1234');
+        expect(user.nickname).toEqual('nickname');
+        expect(user.discord_tag).toEqual('nickname#1234');
+        expect(user.profile_picture).toEqual('/image/img.png');
+        expect(user.cake_day).toEqual(cakeDate);
+        expect(user.birthdate).toEqual(birthDate);
+        expect(user.biography).toEqual('Bio');
+        expect(user.region).toBeNull();
+        expect(user.games).toBeNull();
+        expect(user.languages).toBeNull();
     });
 });
