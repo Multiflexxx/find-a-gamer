81b4d17b77f80e6bafa2919603489dc64caf9b3d
Grimmig18 <benno.grimm@gmx.de>
2020-04-20 00:32:42 +0200

    Add Data_object to Discord-data

---
+++ Source-Code/fagb-backend/src/data_objects/discordinforequest.ts
@@ -0,0 +1,8 @@
+export class DiscordInfoRequest {
+
+    public token: string;
+
+    public constructor(token: string) {
+        this.token = token;
+    }
+}
--- Source-Code/fagb-backend/src/endpoints/discord-data/discord-data.controller.ts
+++ Source-Code/fagb-backend/src/endpoints/discord-data/discord-data.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Post, Body, HttpException, HttpStatus, Get } from '@nestjs/common';
 import { DiscordInformation } from '../../data_objects/discordinformation';
 import { Discord } from '../../factory/discord';
+import { DiscordInfoRequest } from 'src/data_objects/discordinforequest';
 
 @Controller('discord-data')
 export class DiscordDataController {
     @Post()
-    public async getDiscordData(@Body() token: string): Promise<DiscordInformation> {
+    public async getDiscordData(@Body() request: DiscordInfoRequest): Promise<DiscordInformation> {
         // token = "1918e40e-05da-4687-94e9-2aadda531dd8";
         // Validate token format
-        if(!token ||token.length < 36 || !(new RegExp('([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{12})').test(token))) {
+        if(!request.token ||request.token.length < 36 || !(new RegExp('([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{12})').test(request.token))) {
             throw new HttpException({
                 status: HttpStatus.NOT_ACCEPTABLE,
                 error: "Invalid token"
@@ -16,7 +17,7 @@ export class DiscordDataController {
         }
 
         // Get discord Info
-        let discordInfo: DiscordInformation = await Discord.getDiscordInformation(token);
+        let discordInfo: DiscordInformation = await Discord.getDiscordInformation(request.token);
         if(!discordInfo) {
             throw new HttpException({
                 status: HttpStatus.INTERNAL_SERVER_ERROR,
