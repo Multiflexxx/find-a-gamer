72ef0f4008045b40291346eb0c3381dd26883166
TristanMrb <trisemig@aol.com>
2020-04-23 15:14:15 +0200

    Fix

--- Source-Code/fagb-frontend/src/app/match/match-search/match-search.component.ts
+++ Source-Code/fagb-frontend/src/app/match/match-search/match-search.component.ts
@@ -32,7 +32,7 @@ export class MatchSearchComponent implements OnInit {
 
   // Backend input
   public playerList: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  public playstyleList = [
+  public playstyleList: any = [
     {
       name: 'Casual',
       value: 'true'
